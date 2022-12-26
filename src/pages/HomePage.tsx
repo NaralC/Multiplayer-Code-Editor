727b7d094f3e95c715457ab1cf75bbe3d6901b08
Naral <77269201+NaralC@users.noreply.github.com>
2022-12-26 21:52:02 +0000

    Home page generate unique room Id

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,11 +1,28 @@
-import { FC } from "react";
+import { ChangeEvent, FC, useState } from "react";
 import { FaLaptopCode } from "react-icons/fa";
+import { v4 as uuidv4 } from "uuid";
 
 type HomePageProps = {};
 
 const HomePage: FC = (props: HomePageProps) => {
+  const [nickname, setNickname] = useState<string>("");
+  const [roomId, setRoomId] = useState<string>("");
+
+  const createNewRoom = (): void => {
+    const newId = uuidv4();
+    setRoomId(newId);
+  };
+
+  const handleSubmit = (e: ChangeEvent<any>) => {
+    e.preventDefault();
+
+    console.log(nickname);
+    console.log(roomId);
+    // Send something to the backend
+  };
+
   return (
-    <div className="w-screen min-h-screen bg-gray-600 flex flex-col justify-center items-center">
+    <div className="w-screen min-h-screen bg-gray-600 flex flex-col justify-center items-center transition-all delay-75">
       <div className="bg-white p-5 rounded-md flex flex-col gap-5 shadow-black shadow-md">
         <FaLaptopCode className="text-7xl mx-auto" />
         <div className="font-semibold mx-auto">
@@ -24,6 +41,8 @@ const HomePage: FC = (props: HomePageProps) => {
               id="nickname"
               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
               placeholder="Nickname"
+              onChange={(e) => setNickname(e.target.value)}
+              value={nickname}
               required
             />
           </div>
@@ -39,17 +58,23 @@ const HomePage: FC = (props: HomePageProps) => {
               id="roomId"
               placeholder="Room ID"
               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              onChange={(e) => setRoomId(e.target.value)}
+              value={roomId}
               required
             />
           </div>
           <button
             type="submit"
             className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            onSubmit={(e) => handleSubmit(e)}
           >
             Submit
           </button>
         </form>
-        <div className="underline underline-offset-2">
+        <div
+          className="underline underline-offset-2 cursor-pointer"
+          onClick={createNewRoom}
+        >
           If you don't have an invite, click here to generate one.
         </div>
       </div>
