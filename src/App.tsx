d16d3dc5415fd14e4adf032d59149b8f4ec8a608
Naral <77269201+NaralC@users.noreply.github.com>
2022-12-26 22:38:57 +0000

    Add toast for id gen & room navigation

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,28 @@ import { FC } from "react";
 import { Route, Routes } from "react-router-dom";
 import EditorPage from "./pages/EditorPage";
 import HomePage from "./pages/HomePage";
+import { ToastContainer } from "react-toastify";
 import "./App.css";
 
 const App: FC = () => {
   return (
     <div className="">
       <Routes>
-        <Route path="/" element={<HomePage />}/>
-        <Route path="/editor/:roomId" element={<EditorPage />}/>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/editor/:roomId" element={<EditorPage />} />
       </Routes>
+      <ToastContainer
+        position="top-right"
+        autoClose={1000}
+        hideProgressBar
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss={false}
+        draggable={false}
+        pauseOnHover={false}
+        theme="light"
+      />
     </div>
   );
 };
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,24 +1,62 @@
 import { ChangeEvent, FC, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 import { FaLaptopCode } from "react-icons/fa";
 import { v4 as uuidv4 } from "uuid";
 
-type HomePageProps = {};
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-const HomePage: FC = (props: HomePageProps) => {
+const HomePage: FC = () => {
+  const navigate = useNavigate();
   const [nickname, setNickname] = useState<string>("");
   const [roomId, setRoomId] = useState<string>("");
 
   const createNewRoom = (): void => {
     const newId = uuidv4();
     setRoomId(newId);
+
+    toast.success("Created new room", {
+      position: "top-right",
+      autoClose: 1000,
+      hideProgressBar: true,
+      closeOnClick: true,
+      pauseOnHover: false,
+      draggable: false,
+      progress: undefined,
+      theme: "light",
+    });
   };
 
-  const handleSubmit = (e: ChangeEvent<any>) => {
+  const joinRoom = (e: ChangeEvent<any>): void | Promise<void> => {
     e.preventDefault();
 
-    console.log(nickname);
-    console.log(roomId);
-    // Send something to the backend
+    if (!nickname || !roomId) {
+      toast.error("Missing credentials", {
+        position: "top-right",
+        autoClose: 1000,
+        hideProgressBar: true,
+        closeOnClick: true,
+        pauseOnHover: false,
+        draggable: false,
+        progress: undefined,
+        theme: "light",
+      });
+      return;
+    }
+
+    setTimeout(() => {
+      // Send something to the backend here
+
+      
+      navigate(`/editor/${roomId}`, {
+        state: {
+          nickname: nickname,
+          roomId: roomId,
+        },
+      });
+    }, 1000);
+
   };
 
   return (
@@ -37,7 +75,7 @@ const HomePage: FC = (props: HomePageProps) => {
               Your Nickname
             </label>
             <input
-              type="textInput"
+              type="text"
               id="nickname"
               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
               placeholder="Nickname"
@@ -66,7 +104,7 @@ const HomePage: FC = (props: HomePageProps) => {
           <button
             type="submit"
             className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-            onSubmit={(e) => handleSubmit(e)}
+            onClick={(e) => joinRoom(e)}
           >
             Submit
           </button>
