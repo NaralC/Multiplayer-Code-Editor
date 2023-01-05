5e85aa0892060ecac1d855ae4c3a617291eb6237
Naral <77269201+NaralC@users.noreply.github.com>
2023-01-05 21:14:07 +0000

    boilerplate for supported languages

--- client/src/constants/defaultCode.ts
+++ client/src/constants/defaultCode.ts
@@ -1,6 +1,48 @@
+const defaultCode: {
+  [key: string]: string;
+} = {
+  JavaScript: `
+  const twoSum = (nums, target) => {
+    
+};
+`,
+  TypeScript: `
+  const twoSum = (nums: number[], target: number): number[] => {
 
-export const defaultJS = {
-    language: "javascript",
-    defaultCode: "console.log(🤓🤓🤓)",
+};
+    `,
+  Python: `
+  def twoSum(nums, target):
+    
+`,
+  "C++": `
+  class Main {
+    public:
+        vector<int> twoSum(vector<int>& nums, int target) {
+            
+        }
+    };
+`,
+  Java: `
+  public class Main {
+    public static void twoSum(int[] nums, int target) {
+        
+    }
+  }
+`,
+  PHP: `
+  class Main {
+
+    function twoSum($nums, $target) {
+        
+    }
+}
+`,
+  Rust: `
+  pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
     
 }
+`,
+};
+
+export default defaultCode;
