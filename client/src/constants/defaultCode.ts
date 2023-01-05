2ead18f47702802ac2ace026be05f96117208fa7
Naral <77269201+NaralC@users.noreply.github.com>
2023-01-05 23:15:50 +0000

    fix indentation

--- client/src/constants/defaultCode.ts
+++ client/src/constants/defaultCode.ts
@@ -2,44 +2,43 @@ const defaultCode: {
   [key: string]: string;
 } = {
   JavaScript: `
-  const twoSum = (nums, target) => {
+const twoSum = (nums, target) => {
     
 };
 `,
   TypeScript: `
-  const twoSum = (nums: number[], target: number): number[] => {
+const twoSum = (nums: number[], target: number): number[] => {
 
 };
     `,
   Python: `
-  def twoSum(nums, target):
+def twoSum(nums, target):
     
 `,
   "C++": `
-  class Main {
-    public:
-        vector<int> twoSum(vector<int>& nums, int target) {
-            
-        }
-    };
+class Main {
+  public:
+      vector<int> twoSum(vector<int>& nums, int target) {
+          
+      }
+  };
 `,
   Java: `
-  public class Main {
-    public static void twoSum(int[] nums, int target) {
-        
-    }
+public class Main {
+  public static void twoSum(int[] nums, int target) {
+      
   }
+}
 `,
   PHP: `
-  class Main {
-
-    function twoSum($nums, $target) {
-        
-    }
+class Main {
+  function twoSum($nums, $target) {
+      
+  }
 }
 `,
   Rust: `
-  pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
     
 }
 `,
